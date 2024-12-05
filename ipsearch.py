c86cbc94fd323147df51893e8c41364eb58c9597
Alex Yuan <doran@ucdavis.edu>
2024-12-05 01:01:05 -0800

    update

--- ipsearch.py
+++ ipsearch.py
@@ -4,6 +4,6 @@ handler = ipinfo.getHandler(access_token)
 ip_address = '1.1.1.1'
 def do_req(ip_address):
      details = handler.getDetails(ip_address)
-     res="IP地址" + details.ip + "\n城市" + details.city + "\nASN Org:" + details.org + "\n时区" + details.timezone + "\nHostname" + details.hostname
+     res="IP地址" + details.ip + "\n城市" + details.city + "\nASN Org:" + details.org + "\n时区" + details.timezone
 
      return res
